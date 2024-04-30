1d35c0b5912db390ed81d4755e1b170e26ba261d
Hrutik Sargar <hrutiksargar@gmail.com>
2024-04-30 13:20:19 +0530

    add new file

---
+++ src/Java/String/Java_SubString_Comparisons.java
@@ -0,0 +1,92 @@
+package Java.String;
+import java.util.Scanner;
+/*
+We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings
+Input Format
+
+The first line contains a string denoting .
+The second line contains an integer denoting .
+
+Constraints
+
+ consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+Explanation 0
+
+String  has the following lexicographically-ordered substrings of length :
+
+We then return the first (lexicographically smallest) substring and the last (lexicographically largest) substring as two newline-separated values (i.e., ava\nwel).
+
+The stub code in the editor then prints ava as our first line of output and wel as our second line of output.
+*/
+public class Java_SubString_Comparisons {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        String [] data1=new String[s.length()-k+1];
+
+        for (int a=0;a<s.length()-k+1;a++){
+            data1[a]=s.substring(a,k+a);
+        }
+        sort(data1);
+
+        smallest=data1[0];
+        largest=data1[data1.length-1];
+
+
+        return smallest + "\n" + largest;
+    }
+    public  static  String[]  sort(String [] data){
+
+        for (int a=0;a< data.length;a++){
+            for (int b=1;b<data.length-a;b++){
+                if (data[b].compareTo(data[b-1]) <0){
+                    String temp=data[b];
+                    data[b]=data[b-1];
+                    data[b-1]=temp;
+                }
+            }
+        }
+        return data;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
